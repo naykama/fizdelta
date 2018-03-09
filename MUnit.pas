c5f1ad86d4627e964fdc5bf3dce53eea45486468
naykama <naykama@users.noreply.github.com>
2018-03-09 22:49:26 +0400

    add window application project

---
+++ MUnit.pas
@@ -0,0 +1,23 @@
+﻿Unit MUnit;
+
+interface
+
+uses System, System.Drawing, System.Windows.Forms;
+
+type
+  MForm = class(Form)
+  {$region FormDesigner}
+  private
+    {$resource MUnit.MForm.resources}
+    {$include MUnit.MForm.inc}
+  {$endregion FormDesigner}
+  public
+    constructor;
+    begin
+      InitializeComponent;
+    end;
+  end;
+
+implementation
+
+end.
---
+++ fizdelta.pas
@@ -0,0 +1,8 @@
+﻿uses MUnit;
+
+begin
+//  writeln('start...');
+  System.Windows.Forms.Application.EnableVisualStyles();
+  System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+  System.Windows.Forms.Application.Run(new MForm)
+end.
